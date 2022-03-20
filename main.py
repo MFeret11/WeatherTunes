38ab4e36ca6c439695c19fffdb7cbf5a48c1077e
Matthew Feret <mferet11@gmail.com>
2022-03-20 17:16:18 -0400

    Getting each periods temperature converting to single notes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
+import json
 
 if __name__ == '__main__':
-    response = requests.get("https://api.weather.gov/gridpoints/PHI/49,75/forecast").text
+    response = requests.get("https://api.weather.gov/gridpoints/PHI/49,75/forecast")
     print(response)
+    periods = json.loads(response.text)['properties']['periods']
 
+    for period in periods:
+        temperature = period['temperature']
+        note = temperature % 12
+        print(note)
