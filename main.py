c5963a74b9f2904ce59926dcba25982753f56c55
Matthew Feret <mferet11@gmail.com>
2022-03-20 22:31:01 -0400

    Added example of utilizing pyo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from pyo import *
+
 
 if __name__ == '__main__':
     response = requests.get("https://api.weather.gov/gridpoints/PHI/49,75/forecast")
@@ -10,3 +12,26 @@ if __name__ == '__main__':
         temperature = period['temperature']
         note = temperature % 12
         print(note)
+
+    s = Server().boot()
+
+    # Creates a noise source
+    n = Noise()
+
+    # Creates an LFO oscillating +/- 500 around 1000 (filter's frequency)
+    lfo1 = Sine(freq=0.1, mul=500, add=1000)
+    # Creates an LFO oscillating between 2 and 8 (filter's Q)
+    lfo2 = Sine(freq=0.4).range(2, 8)
+    # Creates a dynamic bandpass filter applied to the noise source
+    bp1 = ButBP(n, freq=lfo1, q=lfo2).out()
+
+    # The LFO object provides more waveforms than just a sine wave
+
+    # Creates a ramp oscillating +/- 1000 around 12000 (filter's frequency)
+    lfo3 = LFO(freq=0.25, type=1, mul=1000, add=1200)
+    # Creates a square oscillating between 4 and 12 (filter's Q)
+    lfo4 = LFO(freq=4, type=2).range(4, 12)
+    # Creates a second dynamic bandpass filter applied to the noise source
+    bp2 = ButBP(n, freq=lfo3, q=lfo4).out(1)
+
+    s.gui(locals())
